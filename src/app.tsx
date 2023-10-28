f97a83a1c9d1a69cae425a7a17750da5f9a7c9d6
XyanZhang <1743067175@qq.com>
2023-10-28 23:21:01 +0800

    feat: add logout

--- src/app.tsx
+++ src/app.tsx
@@ -1,50 +1,59 @@
+import { RunTimeLayoutConfig, history } from '@umijs/max';
+import { Modal } from 'antd';
 // 运行时配置
 
-import { LogoutOutlined } from '@ant-design/icons';
-import { Dropdown } from 'antd';
+// import { LogoutOutlined } from '@ant-design/icons';
+// import { Dropdown, theme, ColorPicker  } from 'antd';
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
   return {
-    name: '@umijs/max',
+    name: 'ra-admin',
   };
 }
 
-export const layout = () => {
+// umi 会把 config.ts 中的路由帮我们自动注入到配置的 layout 中，这样我们就可以免去手写菜单的烦恼。
+export const layout: RunTimeLayoutConfig = () => {
   return {
-    // 常用属性
-    title: 'Ant Design',
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-
     menu: {
-      locale: true,
+      locale: false,
     },
-    avatarProps: {
-      src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-      size: 'small',
-      title: '七妮妮',
-      render: (props: any, dom: any) => {
-        return (
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'logout',
-                  icon: <LogoutOutlined />,
-                  label: '退出登录',
-                },
-              ],
-            }}
-          >
-            {dom}
-          </Dropdown>
-        );
-      },
+    fixSiderbar: true,
+    layout: 'mix', //
+    splitMenus: false,
+    // navTheme: 'realDark',
+    contentWidth: 'Fluid',
+    // colorPrimary: $PRIMARY,
+    siderMenuType: 'sub',
+    fixedHeader: true,
+    // 通过token修改主题色
+    // token: {
+    //   sider: {
+    //     colorMenuBackground: '#ffffff',
+    //   },
+    //   header: {
+    //     colorBgHeader: '#001529',
+    //     colorHeaderTitle: '#ffffff',
+    //     heightLayoutHeader: 48,
+    //   },
+    //   pageContainer: {},
+    // },
+    logout: (initialState: any) => {
+      console.log(initialState);
+      Modal.confirm({
+        title: '注销登录',
+        content: '确定要注销登录吗？',
+        okText: '确定',
+        cancelText: '取消',
+        okButtonProps: {
+          danger: true,
+        },
+        onOk: () => {
+          history.replace('/login');
+        },
+      });
     },
-    // 默认布局调整
-    rightContentRender: () => <></>,
-    footerRender: () => <>哈哈</>,
-    menuHeaderRender: undefined,
   };
 };
